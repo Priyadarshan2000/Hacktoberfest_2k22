5021cad937307a61ff3075e62c08a7c2c0a6e821
Chirantan Das <82677314+CHIRANTAN-001@users.noreply.github.com>
2022-10-07 16:20:20 +0530

    Add files via upload

--- DSA/implement_queue_using_stacks.cpp
+++ DSA/implement_queue_using_stacks.cpp
@@ -1,32 +1,50 @@
+// CPP program to implement Queue using
+// two stacks with costly enQueue()
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Queue {
+struct Queue
+{
 	stack<int> s1, s2;
 
 	void enQueue(int x)
 	{
-		while (!s1.empty()) {
+		// Move all elements from s1 to s2
+		while (!s1.empty())
+		{
 			s2.push(s1.top());
 			s1.pop();
 		}
+
+		// Push item into s1
 		s1.push(x);
-		while (!s2.empty()) {
+
+		// Push everything back to s1
+		while (!s2.empty())
+		{
 			s1.push(s2.top());
 			s2.pop();
 		}
 	}
+
+	// Dequeue an item from the queue
 	int deQueue()
 	{
-		if (s1.empty()) {
+		// if first stack is empty
+		if (s1.empty())
+		{
 			cout << "Q is Empty";
 			exit(0);
 		}
+
+		// Return top of s1
 		int x = s1.top();
 		s1.pop();
 		return x;
 	}
 };
+
+// Driver code
 int main()
 {
 	Queue q;
